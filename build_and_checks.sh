8833475db45597990f727dd1ca9bbd4ab15495c4
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2024-04-12 15:10:37 +0200

    LL : licence notice at beginning of file uniformisation to avoid lines above 70 characters.

--- build_and_checks.sh
+++ build_and_checks.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# This file is part of django-monkey-patches library.
+#
+# django-monkey-patches is free software:
+# you can redistribute it and/or modify it under the terms
+# of the GNU Lesser General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License,
+# or (at your option) any later version.
+#
+# django-monkey-patches is distributed in the hope
+# that it will be useful,
+# but WITHOUT ANY WARRANTY;
+# without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of
+# the GNU Lesser General Public License
+# along with django-monkey-patches.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# ©Copyright 2023-2024 Laurent Lyaudet
+
 echo "Building README.md"
 ./build_readme.sh
 
