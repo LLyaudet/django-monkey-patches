19f1e1eb43d2f51c60b720c19905da25ca6fea03
Laurent Lyaudet <laurent.lyaudet@gmail.com>
2024-04-12 15:04:41 +0200

    LL : correction build_and_checks.sh and too_long_code_lines().

--- build_and_checks.sh
+++ build_and_checks.sh
@@ -11,8 +11,27 @@ black .
 echo "Running pylint"
 pylint src/django_monkey_patches/
 
+shopt -s globstar
+
 too_long_code_lines() {
-  grep -r '.\{71\}' {./**.c,./**.css,./**.h,./**.html,./**.js,./**.json,./**.md,./**.php,./**.py,./**.sql,./**.tex,./**.toml,./**.ts,./**.txt,./**.yml,./**COPYING,./**COPYING.LESSER}
+  grep -r '.\{71\}' **/*.c
+  grep -r '.\{71\}' **/*.css
+  grep -r '.\{71\}' **/*.h
+  grep -r '.\{71\}' **/*.htm
+  grep -r '.\{71\}' **/*.html
+  grep -r '.\{71\}' **/*.js
+  grep -r '.\{71\}' **/*.json
+  grep -r '.\{71\}' **/*.md
+  grep -r '.\{71\}' **/*.php
+  grep -r '.\{71\}' **/*.py
+  grep -r '.\{71\}' **/*.sql
+  grep -r '.\{71\}' **/*.tex
+  grep -r '.\{71\}' **/*.toml
+  grep -r '.\{71\}' **/*.ts
+  grep -r '.\{71\}' **/*.txt
+  grep -r '.\{71\}' **/*.yml
+  grep -r '.\{71\}' **/COPYING
+  grep -r '.\{71\}' **/COPYING.LESSER
 }
 
 echo "Analyzing too long lines"
